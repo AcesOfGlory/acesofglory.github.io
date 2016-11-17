57cacc6cb9acac4cd029549b7e85a13728aafd98
Sammy <samnayak@live.co.uk>
2016-11-17 18:52:53 +0000

    Update coordinates-validator.php

--- coordinates-validator.php
+++ coordinates-validator.php
@@ -3,20 +3,10 @@ layout: page
 title: Coordinates Validator
 ---
 
-<html><head>
-<title>IP (Version 4) Address Format Validator</title>
-<meta name="description" content="If you are a software developer or tester of communication applications, you need to know  what is a valid IP addresses and what is an invalid IP address. This page allows you to learn and test valid version 4 IP addresses."> 
-<meta name="keywords" content="Test, IP, Address, Format, Validator, Valid, Invalid">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="stylesheet" type="text/css" href="_style.css">
-</head><body><div class="page">
-
-
-
-
-<form action="coordinates-validator.php" method="post"><p class="query_desc">IP Address: <input name="Query" value="64.233.161.147" type="text" size="50" maxlength="320"></p><p class="query_desc"><input name="Submit" value="Start" type="submit"></p></form></div>
-
-
-
-<div>
-</div></body></html>
+<html>
+  <body>
+    <title>IP (Version 4) Address Format Validator</title>
+  
+    <form action="coordinates-validator.php" method="post"><p class="query_desc">IP Address: <input name="Query" value="64.233.161.147" type="text" size="50" maxlength="320"></p><p class="query_desc"><input name="Submit" value="Start" type="submit"></p></form>
+  </body>
+</html>
