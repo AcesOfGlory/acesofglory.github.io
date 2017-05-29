05e660feb8afa0e8ce24cb61f7a6516e1a8be33f
Sammy <samnayak@live.co.uk>
2017-05-29 13:37:46 +0100

    Update fortuneCookieClicker.js

--- projects/fortune-cookie-clicker/fortuneCookieClicker.js
+++ projects/fortune-cookie-clicker/fortuneCookieClicker.js
@@ -1,5 +1,4 @@
 class Building {
-
 	constructor(name, cost, increment=0, time=1) {
 		this._group = "Building";
 		this._amount = 0;
@@ -10,63 +9,40 @@ class Building {
 	}
 }
 
-document.getElementById("cookie-image").ondragstart = function() { return false; };
+var cookies,
+		cps,
+		currentCookie,
+		buildings,
+		hasBought = false;
 
-var cookies;
-var cps;
-var hasBought = false;
-var currentCookie;
-var buildings;
+function init(){
+	if (typeof(Storage) !== "undefined") {
+		cookies = localStorage.cookies ? Number(localStorage.cookies) : 0;
+		cps = localStorage.cps ? Number(localStorage.cps) : 0;
 
-if(typeof(Storage) !== "undefined") {
-	if (localStorage.cookies) {
-		cookies = Number(localStorage.cookies);
+		if (localStorage.purchases){
+			buildings = JSON.parse(localStorage.getItem("purchases"));
+			hasBought = true;
+		}
+		else {
+			buildings = [
+				new Building("Cursor", 10, 0.25),
+				new Building("Grandma", 100, 1),
+				new Building("Farm", 500, 8),
+				new Building("Mine", 10000, 20),
+				new Building("Factory", 50000, 100),
+				new Building("Bank", 100000, 250),
+				new Building("Temple", 250000, 1000),
+				new Building("Wizard Tower", 500000, 2500),
+				new Building("Shipment", 1000000, 7500),
+				new Building("Alchemy Lab", 5000000, 50000),
+				new Building("Portal", 10000000, 150000),
+				new Building("End Game", 100000000000)
+			];
+		}
 	}
-	else {
-		cookies = 0;
-	}
-	if (localStorage.cps)
-		cps = Number(localStorage.cps);
-	else
-		cps = 0;
 
-	if (localStorage.purchases && 5 == 6){
-		buildings = JSON.parse(localStorage.getItem("purchases"));
-	}
-	else {
-		buildings = [
-			new Building("Cursor", 10, 0.25),
-			new Building("Grandma", 100, 1),
-			new Building("Farm", 500, 8),
-			new Building("Mine", 10000, 20),
-			new Building("Factory", 50000, 100),
-			new Building("Bank", 100000, 250),
-			new Building("Temple", 250000, 1000),
-			new Building("Wizard Tower", 500000, 2500),
-			new Building("Shipment", 1000000, 7500),
-			new Building("Alchemy Lab", 5000000, 50000),
-			new Building("Portal", 10000000, 150000),
-			new Building("End Game", 100000000000)
-		];
-	}
 }
-
-updateCookies();
-
-setInterval(autosave, 30000);
-setInterval(updateCookies, 2000);
-
-buildings.forEach(item => {
-	var para = document.createElement("BR");
-	var card = document.createElement("input");
-	card.type = "button";
-	card.onclick = _ => buy(item);
-	card.id = item._name;
-	card.value = `${item._name}: ${item._amount} (costs ${item._cost})`;
-	document.getElementById("buildings-shop").appendChild(card);
-  document.getElementById("buildings-shop").appendChild(para);
-});
-
 
 function clickCookie(){
 	cookies++;
@@ -76,7 +52,9 @@ function clickCookie(){
 function buy(item){
 	if (!hasBought){
 		hasBought = true;
-		loop();
+		if (timeLoop)
+			clearTimeout(timeLoop);
+		timeLoop = loop();
 	}
 	if (cookies >= item._cost){
 		if (item._name === "End Game"){
@@ -89,7 +67,7 @@ function buy(item){
 		if (item._group == "Building"){
 			item._cost = Math.floor(item._cost * 1.3);
 
-			cps += item._increment/item._time;
+			cps += item._increment / item._time;
 
 			updateCookies();
 			autosave();
@@ -103,7 +81,7 @@ function loop(){
 	cookies += (document.hidden ? cps : cps / 100)
 	//cookies += cps/100
 	document.getElementById("cookie-amount").innerHTML = `Cookies = ${Math.floor(cookies)}`;
-	setTimeout(loop, 10);
+	timeLoop = setTimeout(loop, 10);
 }
 
 function updateCookies(){
@@ -117,4 +95,61 @@ function autosave(){
 	localStorage.cookies = cookies;
 	localStorage.cps = cps;
 	localStorage.setItem("purchases", JSON.stringify(buildings));
+}
+
+function restartGame(){
+	cookies = 0;
+	cps = 0;
+	hasBought = false;
+	if (timeLoop)
+		clearTimeout(timeLoop);
+
+	localStorage.clear();
+	init();
+	buttonCreation();
+
+	buildings.forEach(item => {
+		let input = document.getElementById(item._name);
+		input.value = `${item._name}: ${item._amount} (costs ${item._cost})`;
+	});
+
+	updateCookies();
+	autosave();
+}
+
+function buttonCreation(){
+	var store = document.getElementById("store");
+	store.removeChild(document.getElementById("buildings-shop"));
+
+	var div = document.createElement("div");
+	div.id = "buildings-shop";
+	div.class = "button-column";
+
+	buildings.forEach(item => {
+		var br = document.createElement("br");
+		var input = document.createElement("input");
+		input.type = "button";
+		input.onclick = _ => buy(item);
+		input.id = item._name;
+		input.value = `${item._name}: ${item._amount} (costs ${item._cost})`;
+		div.appendChild(input);
+		div.appendChild(br);
+	});
+
+	document.getElementById("store").appendChild(div);
+}
+
+
+window.onload = function() {
+	document.getElementById("cookie-image").ondragstart = function() { return false; };
+
+	init();
+	buttonCreation();
+	updateCookies();
+	if (hasBought)
+		loop();
+
+	setInterval(autosave, 30000);
+	setInterval(updateCookies, 2000);
+
 }
